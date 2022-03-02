0b6398fef92cdf01fff5d1f6cdf417477160d5db
kirill221100 <ktagilcev@gmail.com>
2022-03-02 11:23:14 +0700

    exchange rate

--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 import requests, datetime
+from forex_python.converter import CurrencyRates
 from flask import flash, session, redirect, request, url_for, render_template
 from app import app
 from app.config import Config as cfg
 from app.forms import OptionsForm
+
+c = CurrencyRates()
+
+
 
 def get_weather_data(city, som):
     if som == standart:
@@ -12,7 +17,7 @@ def get_weather_data(city, som):
     else:
         url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&lang=ru&appid={cfg.API}'
     r = requests.get(url).json()
-    print(r)
+    #print(r)
     return r
 
 standart = {'temp': 'K', 'speed': 'метр/сек.'}
@@ -21,20 +26,22 @@ metric = {'temp': 'C', 'speed': 'метр/сек.'}
 
 @app.route('/', methods=["GET", "POST"])
 def index():
+    usd = round(c.get_rate('USD', 'RUB'), 2)
+    eur = round(c.get_rate('EUR', 'RUB'), 2)
     if session.get('som') == None:
-        session['som'] = standart
+        session['som'] = metric
     if request.method == 'POST':
         data = get_weather_data(request.form['city'], som=session.get('som'))
-        print(data)
+        #print(data)
         if data['cod'] == '404':
             flash('Город не найден :(')
             return redirect(url_for('index'))
         time = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(seconds=data['timezone']))).strftime("%H:%M")
         session['city'] = data['name']
-        return render_template('weather.html', data=data, time=time, som=session.get('som'))
+        return render_template('weather.html', data=data, time=time, som=session.get('som'), usd=usd, eur=eur)
     data = get_weather_data(session.get('city'), som=session.get('som'))
     time = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(seconds=data['timezone']))).strftime("%H:%M")
-    return render_template('weather.html', data=data, time=time, som=session.get('som'))
+    return render_template('weather.html', data=data, time=time, som=session.get('som'), usd=usd, eur=eur)
 
 @app.route('/options', methods=["GET", "POST"])
 def options():

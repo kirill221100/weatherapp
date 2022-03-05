d8b9826cd68528519be1e68f6bf436e5788c77e4
kirill221100 <ktagilcev@gmail.com>
2022-03-05 16:54:31 +0700

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from app.config import Config
+from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
 app.config.from_object(Config)
+scheduler = APScheduler()
+scheduler.init_app(app)
 
 from app.views import *
--- app/config.py
+++ app/config.py
@@ -2,3 +2,6 @@ class Config:
     DEBUG = True
     SECRET_KEY = "secret"
     API = 'secret'
+    CURRENCY_API = 'secret'
+    NEWS_API = 'secret'
+    SCHEDULER_API_ENABLED = True

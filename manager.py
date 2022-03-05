af2eeee47843430bce84149685af66bd5b7f0975
kirill221100 <72173823+kirill221100@users.noreply.github.com>
2022-03-05 17:35:54 +0700

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,7 +1,7 @@
-import random
+# import random
 from app import app
 if __name__ == '__main__':
 
-    port = 5000 + random.randint(0, 999)
-    url = "http://127.0.0.1:{0}".format(port)
+#     port = 5000 + random.randint(0, 999)
+#     url = "http://127.0.0.1:{0}".format(port)
     app.run(use_reloader=False, debug=True, port=port)
